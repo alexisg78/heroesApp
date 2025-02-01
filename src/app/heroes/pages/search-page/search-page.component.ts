464c00921dd17f2aaa7d2ec6a0335c2ad17bf223
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-02-01 13:18:29 -0300

    Implementación de funcionalidad del search-page

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Hero } from '../../interfaces/hero.interface.js';
 import { HeroesService } from '../../services/heroes.service.js';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
@@ -12,8 +14,9 @@ export class SearchPageComponent {
 
   public searchInput= new FormControl('')
   public heroes: Hero[] = [];
+  public selectedHero?: Hero;
 
-  constructor( private heroService: HeroesService){}
+  constructor( private heroService: HeroesService, private router: Router){}
 
   searchHero() {
     const value: string = this.searchInput.value || '';
@@ -22,7 +25,16 @@ export class SearchPageComponent {
       .subscribe( heroes => this.heroes= heroes )
   }
 
-  onSelectionOption(){
-    // terminar
+  onSelectionOption( event : MatAutocompleteSelectedEvent ): void {
+
+    if ( !event.option.value ){
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero );
+    this.selectedHero= hero;
+    this.router.navigate(['/heroes', this.selectedHero.id]);
   }
 }
